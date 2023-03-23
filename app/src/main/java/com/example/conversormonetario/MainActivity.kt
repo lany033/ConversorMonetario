08e22eef37c96cc911b7c527f044baa0dd2fe2de
lany033 <mmantilla930@gmail.com>
2023-03-23 10:46:25 -0500

    UI finish without basic logic

--- app/src/main/java/com/example/conversormonetario/MainActivity.kt
+++ app/src/main/java/com/example/conversormonetario/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.conversormonetario
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.example.conversormonetario.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val divisas = resources.getStringArray(R.array.divisas)
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.list_item,
+            divisas
+        )
+
+        with(binding.autoCompleteTextView){
+            setAdapter(adapter)
+            onItemClickListener = this@MainActivity
+        }
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val item1 = parent?.getItemAtPosition(position)
     }
 }
