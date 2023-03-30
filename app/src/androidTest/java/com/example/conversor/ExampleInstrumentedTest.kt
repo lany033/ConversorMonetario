9862020c2c968d96f1d4763251883d1a6ce63aa3
lany033 <mmantilla930@gmail.com>
2023-03-29 20:35:15 -0500

    fragment done

--- app/src/androidTest/java/com/example/conversor/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/conversor/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.conversormonetario
+package com.example.conversor
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/main/java/com/example/conversor/ui/MainActivity.kt
+++ app/src/main/java/com/example/conversor/ui/MainActivity.kt
@@ -1,28 +1,31 @@
-package com.example.conversormonetario
+package com.example.conversor.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.*
-import com.example.conversormonetario.databinding.ActivityMainBinding
-import com.example.conversormonetario.model.CurrencyResultData
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.conversor.ApiLayerService
+import com.example.conversor.R
+import com.example.conversor.databinding.ActivityMainBinding
 import retrofit2.*
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     //We use lazy to make sure the instances are only created when needed.
-    private val serverResponseView: TextView by lazy {
-        binding.result
-    }
+    //private val serverResponseView: TextView by lazy { binding.result}
+    /*
     private lateinit var amount: EditText
     private lateinit var item1: String
     private lateinit var item2: String
 
     private lateinit var dropDownMenu1: AutoCompleteTextView
     private lateinit var dropDownMenu2: AutoCompleteTextView
+
+     */
 
 
     private val retrofit by lazy {
@@ -36,9 +39,6 @@ class MainActivity : AppCompatActivity() {
         retrofit.create(ApiLayerService::class.java)
     }
 
-    companion object {
-        private const val APP_ID = "ermJivbq91H7RNv7QRYuc4WBDvFf0F7g"
-    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -47,7 +47,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        amount = binding.value1
+
+        val bottomNavigationView = binding.bottomNavView
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
 
         val divisas1 = resources.getStringArray(R.array.divisas)
         val divisas2 = resources.getStringArray(R.array.divisas)
@@ -64,9 +71,8 @@ class MainActivity : AppCompatActivity() {
             divisas2
         )
 
-        dropDownMenu1 = binding.autoCompleteTextView
-        dropDownMenu2 = binding.autoCompleteTextView2
 
+/*
         with(dropDownMenu1){
             setAdapter(adapter1)
         }
@@ -83,11 +89,12 @@ class MainActivity : AppCompatActivity() {
             onDropDowmItemClick2(parent,view,position,id)
         }
 
-        binding.buttonConvertir.setOnClickListener {
-            getConvertFunctionResponse()
-        }
+ */
+
+
     }
 
+    /*
     private fun onDropDowmItemClick1(parent: AdapterView<*>, view: View?, position: Int, id: Long){
 
         item1 = parent.getItemAtPosition(position).toString()
@@ -114,6 +121,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    */
+/*
     private fun getConvertFunctionResponse(){
         val call = apiLayerService.searchConvert("ermJivbq91H7RNv7QRYuc4WBDvFf0F7g",item2,item1,amount.text.toString().toBigDecimal())
         call.enqueue(object: Callback<CurrencyResultData> {
@@ -125,7 +134,7 @@ class MainActivity : AppCompatActivity() {
                 if (response.isSuccessful){
                     val currencyResult = response.body()
                     val resultado = currencyResult?.result
-                    serverResponseView.text = "Result: $resultado $item2 "
+                    //serverResponseView.text = "Result: $resultado $item2 "
                 } else {
                     Log.e("MainActivity",
                     "Failed to get search result\n ${response.errorBody()?.string() ?: ""}"
@@ -134,5 +143,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+ */
 
 }
